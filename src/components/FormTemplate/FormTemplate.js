34c3cd17fc999758ea299f4c02bda3ff52bee8a0
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-13 09:52:03 +0545

    Add function form template

--- src/components/FormTemplate/FormTemplate.js
+++ src/components/FormTemplate/FormTemplate.js
@@ -1,2 +1,16 @@
 import React from "react";
 
+function FormTemplate({title, fields, data, setData, onSubmit}){
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setData(prevData => ({
+            ...prevData, [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(data);
+    };
+
+}
