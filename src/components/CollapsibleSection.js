3951470600dcf5124c3ae9742ca578311e6ad903
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-13 10:56:04 +0545

    Add function inputGroup

--- src/components/CollapsibleSection.js
+++ src/components/CollapsibleSection.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 
+//function to make a section collapsible
+
 function CollapsibleSection({title, children}){
     const [isOpen, setIsOpen] = useState(false);
 
