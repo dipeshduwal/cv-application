ff0d07fe09b1f1d1f2426ecc05ae4f44fc267186
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-16 16:35:21 +0545

    Add default data to load template

--- src/defaultData.js
+++ src/defaultData.js
@@ -2,19 +2,19 @@ import uniqid from "uniqid";
 
 const defaultData = {
     personalInfo: {
-        fullName: "Max Müller",
-        email: "max.mueller@gmail.com",
-        phoneNumber: "+79 100 20 20",
-        address: "Bellikon, AG",
-        birthDate: "01.01.2000",
+        fullName: "Dipesh Duwal",
+        email: "dipeshduwal@gmail.com",
+        phoneNumber: "+977-9813968406",
+        address: "Dekocha, Bhaktapur",
+        birthDate: "05.06.1999",
     },
 
     sections: {
         educations: [
             {
-                degree: "Kaufmann EFZ",
-                schoolName: "zB. Zentrum Bildung",
-                location: "Baden",
+                degree: "BSc. Computing (Hons.)",
+                schoolName: "Islington College",
+                location: "Kamalpokhari",
                 startDate: "08/2020",
                 endDate: "06/2023",
                 isCollapsed: true,
@@ -25,11 +25,11 @@ const defaultData = {
 
         experiences: [
             {
-                companyName: "Firma XYZ",
-                positionTitle: "Lernender Kaufmann",
-                location: "Bellikon",
+                companyName: "OpenCafe LLC",
+                positionTitle: "Software Developer Intern",
+                location: "Lalitpur",
                 description:
-                    "Administrative und kaufmännische Aufgaben in einem Betrieb der Baubranche",
+                    "Make Awesome Applications Usign Various Tools and Technologies Like React and Node.",
                 startDate: "08/2020",
                 endDate: "present",
                 isCollapsed: true,
