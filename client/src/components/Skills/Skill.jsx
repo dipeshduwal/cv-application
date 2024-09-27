7c165d24a535cfc19733a4e647b6e9dd861c8bda
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-09-27 14:38:40 +0545

    Integrate skill route to componenet

--- client/src/components/Skills/Skill.jsx
+++ client/src/components/Skills/Skill.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import FormTemplate from "../FormTemplate/FormTemplate";
 import ItemTemplate from "../FormTemplate/ItemTemplate";
 import '../../styles/Buttons.css'
