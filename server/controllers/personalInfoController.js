77fa9e272191b66462da8b4080c632a490166fb9
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-10-02 14:02:29 +0545

    Add personalInfoController

--- server/controllers/personalInfoController.js
+++ server/controllers/personalInfoController.js
@@ -8,3 +8,30 @@ exports.GetInfo = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.PostInfo = async (req, res) => {
+    try {
+        const personalInfo = await PersonalInfo.upsert(req.body); // Upsert for either insert or update
+        res.status(201).json(personalInfo);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.DeleteInfo = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await PersonalInfo.destroy({
+            where: { id }
+        });
+        
+        if (result) {
+            res.status(200).json({ message: 'Personal info deleted successfully.' });
+        } else {
+            res.status(404).json({ message: 'Personal info not found.' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
--- server/routes/personalInfoRoute.js
+++ server/routes/personalInfoRoute.js
@@ -1,42 +1,12 @@
 const express = require('express');
-const PersonalInfo = require('../models/personalInfo');
+const {GetInfo, PostInfo, DeleteInfo} = require('../controllers/personalInfoController');
+
 const router = express.Router();
 
-// Get personal info
-router.get('/', async (req, res) => {
-    try {
-        const personalInfo = await PersonalInfo.findOne();
-        res.status(200).json(personalInfo);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+router.get('/', GetInfo);
 
-// Create or update personal info
-router.post('/', async (req, res) => {
-    try {
-        const personalInfo = await PersonalInfo.upsert(req.body); // Upsert for either insert or update
-        res.status(201).json(personalInfo);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+router.post('/', PostInfo);
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const result = await PersonalInfo.destroy({
-            where: { id }
-        });
-        
-        if (result) {
-            res.status(200).json({ message: 'Personal info deleted successfully.' });
-        } else {
-            res.status(404).json({ message: 'Personal info not found.' });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+router.delete('/:id', DeleteInfo);
 
 module.exports = router;
