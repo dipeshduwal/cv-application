4c10c159bd705c5d0d9b94070b57ffa451e4598a
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-10-22 10:42:04 +0545

    Apply errorHandlerWrapper to password routes

--- server/controllers/passwordController.js
+++ server/controllers/passwordController.js
@@ -1,26 +1,15 @@
 const { requestPasswordReset, resetPassword } = require('../services/passwordServices');
-const { handleServerError } = require('../utils/serverErrorHandler');
 
 // Request Password Reset Controller
 exports.requestPasswordReset = async (req, res) => {
-    try {
         const { email } = req.body;
         await requestPasswordReset(email);
         res.status(200).json({ message: 'OTP sent to your email' });
-    } catch (error) {
-        console.error('Error in requestPasswordReset:', error);
-        handleServerError(res, error);
-    }
 };
 
 // Reset Password Controller
 exports.resetPassword = async (req, res) => {
-    try {
         const { email, otp, newPassword } = req.body;
         await resetPassword(email, otp, newPassword);
         res.status(200).json({ message: 'Password reset successfully' });
-    } catch (error) {
-        console.error('Error in resetPassword:', error);
-        handleServerError(res, error);
-    }
 };
--- server/routes/passwordRoute.js
+++ server/routes/passwordRoute.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require('express')
+const errorHandlerWrapper = require('../middleware/errorHandlerWrapper');
 const { requestPasswordReset, resetPassword } = require('../controllers/passwordController');
 
 const router = express.Router();
 
 // Forgot Password - Request OTP
-router.post('/forgot-password', requestPasswordReset);
+router.post('/forgot-password', errorHandlerWrapper(requestPasswordReset));
 
 // Reset Password using OTP
-router.post('/reset-password', resetPassword);
+router.post('/reset-password', errorHandlerWrapper(resetPassword));
 
 module.exports = router;
