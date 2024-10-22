bf9710395657f1067a754882cdbaa9bd34f394a1
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-10-22 10:37:33 +0545

    Apply errorHandlerWrapper to Auth Routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,27 +1,15 @@
 const { signup, login } = require('../services/authServices');
-const { handleServerError } = require('../utils/serverErrorHandler');
-
 
 // Signup Controller
 exports.signup = async (req, res) => {
     const { username, email, password } = req.body;
-
-    try {
-        const result = await signup(username, email, password);
-        res.status(201).json(result);
-    } catch (err) {
-        handleServerError(res, err);
-    }
+    const result = await signup(username, email, password);
+    res.status(201).json(result);
 };
 
 // Login Controller
 exports.login = async (req, res) => {
     const { email, password } = req.body;
-
-    try {
-        const result = await login(email, password);
-        res.status(200).json(result);
-    } catch (err) {
-        handleServerError(res, err); 
-    }
+    const result = await login(email, password);
+    res.status(200).json(result);
 };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const errorHandlerWrapper = require('../middleware/errorHandlerWrapper');
 const { signup, login } = require('../controllers/authController');
 
 const router = express.Router();
 
 // Signup Route
-router.post('/signup', signup);
+router.post('/signup', errorHandlerWrapper(signup));
 
 // Login Route
-router.post('/login', login);
+router.post('/login', errorHandlerWrapper(login));
 
 module.exports = router;
