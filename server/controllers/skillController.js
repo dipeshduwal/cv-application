acbcc2b341e3c6742e10481c14fe877bfdeec61a
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-10-22 10:47:44 +0545

    Apply errorHandlerWrapper to skill routes

--- server/controllers/skillController.js
+++ server/controllers/skillController.js
@@ -1,21 +1,14 @@
 const { createSkill, updateSkill, deleteSkill, getSkill} = require('../services/skillServices');
-const { handleServerError } = require('../utils/serverErrorHandler');
 
 exports.PostSkill = async (req, res) => {
-    try {
         const skillDetails = req.body;
         const { email } = req.user; 
 
         const newSkill =  await createSkill(email, skillDetails);
         return res.status(201).json(newSkill);
-    } catch (error) {
-        console.error('PostSkill Error:', error);
-        handleServerError(res, error);
-    }
 };
 
 exports.GetSkill = async (req, res) => {
-    try {
         const { email } = req.user;
         const skillList = await getSkill(email);
 
@@ -23,26 +16,16 @@ exports.GetSkill = async (req, res) => {
             return res.status(404).json({ message: "No skill records found"});
         }
         return res.status(200).json(skillList);
-    } catch (error) {
-        console.error('GetSkill Error:', error);
-        handleServerError(res, error);
-    }
 };
 
 exports.PutSkill = async (req, res) => {
-    try {
         const { email } = req.user;
         const { skillId, ...skillDetails} = req.body;
         const updatedSkill = await updateSkill(email, skillId, skillDetails);
         return res.status(200).json(updatedSkill);
-    } catch (error) {
-        console.error('PutSkill Error:', error);
-        handleServerError(res, error);
-    }
 };
 
 exports.DeleteSkill = async (req, res) => {
-    try {
         const skillId = req.params.id;  
 
         // Extract the user's unique identifier (email) from the token
@@ -50,8 +33,4 @@ exports.DeleteSkill = async (req, res) => {
         const result = await deleteSkill(email, skillId);
 
         return res.status(200).json(result);
-    } catch (error) {
-        console.error('DeleteSkill Error:', error);
-        handleServerError(res, error);
-    }
 };
--- server/routes/skillRoute.js
+++ server/routes/skillRoute.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware'); 
 const {PostSkill, GetSkill, PutSkill, DeleteSkill} = require('../controllers/skillController');
+const errorHandlerWrapper = require('../middleware/errorHandlerWrapper');
 
 const router = express.Router();
 
-router.post('/', authMiddleware, PostSkill);
+router.post('/', authMiddleware, errorHandlerWrapper(PostSkill));
 
-router.get('/', authMiddleware, GetSkill);
+router.get('/', authMiddleware, errorHandlerWrapper(GetSkill));
 
-router.put('/:id', authMiddleware, PutSkill);
+router.put('/:id', authMiddleware, errorHandlerWrapper(PutSkill));
 
-router.delete('/:id', authMiddleware, DeleteSkill);
+router.delete('/:id', authMiddleware, errorHandlerWrapper(DeleteSkill));
 
 module.exports = router;
