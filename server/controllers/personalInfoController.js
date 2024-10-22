df276ffc3635d939a70450b8809f79919822a959
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-10-22 10:44:16 +0545

    Apply errorHandlerWrapper to personalInfoRoute

--- server/controllers/personalInfoController.js
+++ server/controllers/personalInfoController.js
@@ -3,26 +3,19 @@ const {
     getInfo,
     deleteInfo,
 } = require('../services/personalInfoServices');
-const { handleServerError } = require('../utils/serverErrorHandler');
 
 // Create or update personal info
 exports.PostInfo = async (req, res) => {
-    try {
         const { email: userEmail } = req.user;
         const info = req.body;
         const file = req.files?.profileImage;
 
         const updatedInfo = await createOrUpdateInfo(userEmail, info, file);
         res.status(201).json(updatedInfo);
-    } catch (error) {
-        console.error('Error in PostInfo:', error);
-        handleServerError(res, error);
-    }
 };
 
 // Get personal info
 exports.GetInfo = async (req, res) => {
-    try {
         const { email: userEmail } = req.user;
         const personalInfo = await getInfo(userEmail);
 
@@ -30,20 +23,11 @@ exports.GetInfo = async (req, res) => {
             return res.status(404).json({ message: "Personal info not found" });
         }
         res.status(200).json(personalInfo);
-    } catch (error) {
-        console.error('Error in GetInfo:', error);
-        handleServerError(res, error);
-    }
 };
 
 // Delete personal info
 exports.DeleteInfo = async (req, res) => {
-    try {
         const { email: userEmail } = req.user;
         const result = await deleteInfo(userEmail);
         res.status(200).json(result);
-    } catch (error) {
-        console.error('Error in DeleteInfo:', error);
-        handleServerError(res, error);
-    }
 };
--- server/routes/personalInfoRoute.js
+++ server/routes/personalInfoRoute.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const {GetInfo, PostInfo, DeleteInfo} = require('../controllers/personalInfoController');
 const authMiddleware = require('../middleware/authMiddleware'); 
+const errorHandlerWrapper = require('../middleware/errorHandlerWrapper');
 
 const router = express.Router();
 
-router.get('/', authMiddleware, GetInfo);
+router.get('/', authMiddleware, errorHandlerWrapper(GetInfo));
 
-router.post('/', authMiddleware, PostInfo);
+router.post('/', authMiddleware, errorHandlerWrapper(PostInfo));
 
-router.delete('/:id', authMiddleware, DeleteInfo);
+router.delete('/:id', authMiddleware, errorHandlerWrapper(DeleteInfo));
 
 module.exports = router;
