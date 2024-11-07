d40434ab21d6b11db958ac534505773ef746feb3
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-11-07 14:06:51 +0545

    Modify css to suit theme

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './pages/login/login'
+import { FaSun, FaMoon } from 'react-icons/fa';
+import Login from './pages/login/login';
 import Signup from './pages/signUp/signUp';
 import HomePage from './pages/homePage/homePage';
 import ProfileComponent from './pages/profileComponent/profileComponent';
@@ -10,31 +11,46 @@ import CVApp from './pages/cvApp/cvApp';
 import ForgotPassword from './pages/forgotPassword/forgotPassword';
 import ResetPassword from './pages/resetPassword/resetPassword';
 import VerifyOtp from './pages/emailVerification/emailVerification';
-import { ThemeProvider } from './contexts/themeContext/themeContext';
+import { ThemeContext, ThemeProvider } from './contexts/themeContext/themeContext';
+import './styles/app.css';
 
 function App() {
   return (
-    <ThemeProvider>
-    <Router>
-      <div className='App'>
+    <ThemeProvider> {/* ThemeProvider should wrap the whole app */}
+      <AppContent /> {/* AppContent will now use the context */}
+    </ThemeProvider>
+  );
+}
+
+function AppContent() {
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <div className='App'>
+      {/* Theme toggle button */}
+      <div onClick={toggleTheme} className="theme-toggle-btn">
+        {isDarkMode ? <FaSun /> : <FaMoon />}
+      </div>
+
+      {/* Routing setup */}
+      <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<ProfileComponent />} />
-          <Route path="/faq" element={<FAQ/>}/>
+          <Route path="/faq" element={<FAQ />} />
           <Route path="/cvapp" element={<CVApp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/verify-otp" element={<VerifyOtp/>}/>
-          <Route path="/contact" element={<Contact />} /> 
+          <Route path="/verify-otp" element={<VerifyOtp />} />
+          <Route path="/contact" element={<Contact />} />
 
           {/* Redirect to homepage for any unmatched routes */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </div>
-    </Router>
-    </ThemeProvider>
+      </Router>
+    </div>
   );
 }
 
